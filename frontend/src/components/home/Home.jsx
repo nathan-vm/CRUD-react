41820c9567e41eb485271ef7ac64395ee3e34f8e
nathan-vm <nathan.v.marcelino@gmail.com>
2020-02-27 16:21:23 -0300

    Home feita

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props=>
+<Main icon="home" title="Início" 
+subtitle="Projeto de cadastro de usuarios usando react ">
+    <div className="display-4"> Bem Vindo!</div>
+    <hr/>
+    <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em react</p>
+</Main>
