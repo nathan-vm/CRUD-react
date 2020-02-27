9948125d445dd673ed3c828aae2ab06b01fa106b
nathan-vm <nathan.v.marcelino@gmail.com>
2020-02-27 16:08:03 -0300

    Menu

--- frontend/src/components/template/Nav.jsx
+++ frontend/src/components/template/Nav.jsx
@@ -1,7 +1,16 @@
 import './Nav.css'
 import React from 'react'
 
+{/* CRIAR UM COMPONENTE PARA CADA "BOTAO" DE MENU */}
+
 export default props=>
     <aside className="menu-area">
-        Navegação
+        <nav className="menu">
+            <a href="#">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+        </nav>
     </aside>
